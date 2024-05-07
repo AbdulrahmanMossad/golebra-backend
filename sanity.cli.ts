c47d3869550a94411aca9cacbf89a20972c48b06
AbdulrahmanMossad <abdulrahmanmosad670@gmail.com>
2024-05-07 11:58:18 +0300

    golebra-backend

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '2gc8t8cc',
+    dataset: 'production'
+  }
+})
---
+++ schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import pin from './pin'
+import user from './user'
+import postedBy from './postedBy'
+import save from './save'
+import comment from './comment'
+
+export const schemaTypes = [user, save, postedBy, comment, pin]
